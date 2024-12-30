3140d9cab2c8faf467cb66e650c766d51ae56404
William Train <William.Train@ametek.com>
2024-12-30 16:07:44 +0000

    Converting from canvas pixel coordinates to clip space in vertex shader. Also moving shader compiling and program linking to it's own JS file

---
+++ www/modules/webgl-utilities.js
@@ -0,0 +1,56 @@
+const name = 'webgl-utilities';
+
+// Builds and links a shader program
+function buildShaderProgram(gl, shaderInfo) {
+    const program = gl.createProgram();
+
+    shaderInfo.forEach((info) => {
+        const shader = compileShader(gl, info.type, info.source);
+
+        if (shader) {
+            gl.attachShader(program, shader);
+        }
+    });
+
+    gl.linkProgram(program);
+
+    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+        console.log("Error linking shader program");
+        console.log(gl.getProgramInfoLog(program));
+    }
+
+    return program;
+}
+
+function compileShader(gl, type, source) {
+    const shader = gl.createShader(type);
+
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        console.log(
+            `Error compiling ${
+                type === gl.VERTEX_SHADER ? "vertex" : "fragment"
+            } shader:`
+        );
+        console.log(gl.getShaderInfoLog(shader));
+    }
+    return shader;
+}
+
+function getProgramLocations(gl, program) {
+    var programInfo = {
+        program: program,
+        attribLocations: {
+            vertexPosition: gl.getAttribLocation(program, "aVertexPosition"),
+        },
+        uniformLocations: {
+            uResolution: gl.getUniformLocation(program, "uResolution"),
+            uGlobalColor: gl.getUniformLocation(program, "uGlobalColor"),
+        }
+    };
+    return programInfo;
+}
+
+export { buildShaderProgram, compileShader, getProgramLocations }
--- www/modules/webgl.js
+++ www/modules/webgl.js
@@ -1,3 +1,5 @@
+import { buildShaderProgram, compileShader, getProgramLocations } from "./webgl-utilities.js"
+
 const name = 'webgl';
 
 let gl = null;
@@ -7,10 +9,11 @@ let aspectRatio;
 let scale = [1.0, 1.0];
 
 // Vertex info
-let vertexArray;
+let vertices;
 let vertexBuffer;
 let vertexNumComponents = 2;
 let vertexCount;
+let vao;
 
 let shaderProgram;
 let programInfo;
@@ -22,15 +25,16 @@ function startup() {
     const shaderSet = [
         { 
             type: gl.VERTEX_SHADER,
-            id: "vertex-shader",
+            source: document.getElementById("vertex-shader").firstChild.nodeValue,
         },
         {
             type: gl.FRAGMENT_SHADER,
-            id: "fragment-shader",
+            source: document.getElementById("fragment-shader").firstChild.nodeValue,
         }
     ];
 
-    shaderProgram = buildShaderProgram(shaderSet);
+    shaderProgram = buildShaderProgram(gl, shaderSet);
+    programInfo = getProgramLocations(gl, shaderProgram);
     
     aspectRatio = glCanvas.width / glCanvas.height;
     scale = [0.5, 0.5];
@@ -38,66 +42,8 @@ function startup() {
     gl.clearColor(0, 0, 0, 0);
     gl.clear(gl.COLOR_BUFFER_BIT);
 
-    vertexBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-
-    // create array of vertices for a square
-    vertexArray = [0.5, 0.5, -0.5, 0.5, 0.5, -0.5, -0.5, -0.5];
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexArray), gl.STATIC_DRAW);
-
-    vertexCount = vertexArray.length / vertexNumComponents;
-
+    createVertexBuffer();
     setPositionAttribute();
-}
-
-function buildShaderProgram(shaderInfo) {
-    const program = gl.createProgram();
-
-    shaderInfo.forEach((info) => {
-        const shader = compileShader(info.id, info.type);
-
-        if (shader) {
-            gl.attachShader(program, shader);
-        }
-    });
-
-    gl.linkProgram(program);
-
-    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-        console.log("Error linking shader program");
-        console.log(gl.getProgramInfoLog(program));
-    }
-
-    programInfo = {
-        program: program,
-        attribLocations: {
-            vertexPosition: gl.getAttribLocation(program, "aVertexPosition"),
-        },
-        uniformLocations: {
-            uScalingFactor: gl.getUniformLocation(program, "uScalingFactor"),
-            uGlobalColor: gl.getUniformLocation(program, "uGlobalColor"),
-        }
-    };
-
-    return program;
-}
-
-function compileShader(id, type) {
-    const source = document.getElementById(id).firstChild.nodeValue;
-    const shader = gl.createShader(type);
-
-    gl.shaderSource(shader, source);
-    gl.compileShader(shader);
-
-    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-        console.log(
-            `Error compiling ${
-                type === gl.VERTEX_SHADER ? "vertex" : "fragment"
-            } shader:`
-        );
-        console.log(gl.getShaderInfoLog(shader));
-    }
-    return shader;
 }
 
 function onFrame() {
@@ -105,22 +51,46 @@ function onFrame() {
     gl.clear(gl.COLOR_BUFFER_BIT);
 
     gl.useProgram(shaderProgram);
-    gl.uniform2fv(programInfo.uniformLocations.uScalingFactor, scale);
+    gl.bindVertexArray(vao);
+    gl.uniform2f(programInfo.uniformLocations.uResolution, gl.canvas.width, gl.canvas.height);
     gl.uniform4fv(programInfo.uniformLocations.uGlobalColor, [0.1, 0.7, 0.2, 1.0]);
 
     gl.drawArrays(gl.TRIANGLE_STRIP, 0, vertexCount)
 }
 
+function createVertexBuffer() {
+    // Create and bind vertex buffer
+    vertexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+
+    // create array of vertices for a square
+    vertices = [80, 80, 20, 80, 80, 20, 20, 20];
+    // Fill vertex buffer with data
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+
+    vertexCount = vertices.length / vertexNumComponents;
+}
+
 function setPositionAttribute() {
+    gl.createVertexArray();
+    gl.bindVertexArray(vao);
     gl.enableVertexAttribArray(programInfo.attribLocations.vertexPosition);
+    var size = vertexNumComponents;     // 2 components per vertex
+    var type = gl.FLOAT;                // the data is 32 bit floats
+    var normalize = false;              // don't normalize the data
+    var stride = 0;                     // 0 = stride between vertices is calculated from size * sizeof(type)
+    var offset = 0;                     // start at beginning of buffer
     gl.vertexAttribPointer(
         programInfo.attribLocations.vertexPosition,
-        vertexNumComponents,
-        gl.FLOAT,
-        false,
-        0,
-        0
+        size,
+        type,
+        normalize,
+        stride,
+        offset
     );
+    // N.B. gl.vertexAttribPointer also binds current ARRAY_BUFFER to the attribute,
+    // so we can now bind something else to ARRAY_BUFFER if we want, the attribute
+    // will continue to use vertexBuffer
 }
 
 export { startup, onFrame };
