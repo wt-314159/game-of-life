a87b013d793ef6502ac3cde42a4afec3b46ab72b
William Train <William.Train@ametek.com>
2024-12-31 17:29:40 +0000

    Changing size of benchmark universe to be bigger, to compare how different approaches scale. Hypothesis is that with larger universes, the overhead of allocation will diminish

--- benches/bench.rs
+++ benches/bench.rs
@@ -5,7 +5,7 @@ extern crate game_of_life;
 
 #[bench]
 fn universe_ticks(b: &mut test::Bencher) {
-    let mut universe = game_of_life::Universe::new(128, 128);
+    let mut universe = game_of_life::Universe::new(500, 500);
 
     b.iter(|| {
         universe.tick();
