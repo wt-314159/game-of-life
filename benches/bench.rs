60bdcd4faeded7ed2275736b60905ed3a5a95050
William Train <William.Train@ametek.com>
2025-01-02 02:17:17 +0000

    Switching to using Criterion for benching

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,37 +1,40 @@
-#![feature(test)]
-#![cfg(not(target_arch = "wasm32"))]
+#[allow(unused_imports)]
+use std::hint::black_box;
+use criterion::{criterion_group, criterion_main, Criterion};
 
-extern crate test;
 extern crate game_of_life;
 
-#[bench]
-fn universe_ticks(b: &mut test::Bencher) {
+fn benchmark(c: &mut Criterion) {
     let mut universe = game_of_life::Universe::new(800, 800);
 
-    b.iter(|| {
-        universe.tick();
-    })
+    c.bench_function(
+        "tick", 
+        |b| b.iter(|| {
+            universe.tick();
+    }));
 }
 
-#[bench]
-fn universe_live_neighbours(b: &mut test::Bencher) {
-    let (width, height) = (500, 500);
-    let universe = game_of_life::Universe::new_rand(width, height);
-    let cells = universe.get_cells();
+// fn universe_live_neighbours(b: &mut test::Bencher) {
+//     let (width, height) = (500, 500);
+//     let universe = game_of_life::Universe::new_rand(width, height);
+//     let cells = universe.get_cells();
     
-    let last_row = height - 1;
-    let last_col = width - 1;
-    let mid_row = height / 2;
-    let mid_col = width / 2;
+//     let last_row = height - 1;
+//     let last_col = width - 1;
+//     let mid_row = height / 2;
+//     let mid_col = width / 2;
 
-    let mut count = 0;
-    b.iter(|| {
-        count += game_of_life::Universe::live_neighbour_count(width, height, cells, 0, 0);
-        count += game_of_life::Universe::live_neighbour_count(width, height, cells, 1, 1);
-        count += game_of_life::Universe::live_neighbour_count(width, height, cells, 2, 2);
-        count += game_of_life::Universe::live_neighbour_count(width, height, cells, mid_row, mid_col);
-        count += game_of_life::Universe::live_neighbour_count(width, height, cells, last_row, last_col);
-        count += game_of_life::Universe::live_neighbour_count(width, height, cells, last_row - 1, last_col - 1);
-        count += game_of_life::Universe::live_neighbour_count(width, height, cells, last_row - 2, last_col - 2);
-    })
-}
+//     let mut count = 0;
+//     b.iter(|| {
+//         count += game_of_life::Universe::live_neighbour_count(width, height, cells, 0, 0);
+//         count += game_of_life::Universe::live_neighbour_count(width, height, cells, 1, 1);
+//         count += game_of_life::Universe::live_neighbour_count(width, height, cells, 2, 2);
+//         count += game_of_life::Universe::live_neighbour_count(width, height, cells, mid_row, mid_col);
+//         count += game_of_life::Universe::live_neighbour_count(width, height, cells, last_row, last_col);
+//         count += game_of_life::Universe::live_neighbour_count(width, height, cells, last_row - 1, last_col - 1);
+//         count += game_of_life::Universe::live_neighbour_count(width, height, cells, last_row - 2, last_col - 2);
+//     })
+// }
+
+criterion_group!(benches, benchmark);
+criterion_main!(benches);
