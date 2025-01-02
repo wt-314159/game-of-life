6fb312c30bfb5991b0137fea1d26481bb799f023
William Train <William.Train@ametek.com>
2025-01-02 21:35:17 +0000

    Bench testing Universe::live_neighbours_count()

--- benches/bench.rs
+++ benches/bench.rs
@@ -4,8 +4,10 @@ use criterion::{criterion_group, criterion_main, Criterion};
 
 extern crate game_of_life;
 
-fn benchmark(c: &mut Criterion) {
-    let mut universe = game_of_life::Universe::new(800, 800);
+#[allow(dead_code)]
+fn tick_benchmark(c: &mut Criterion) {
+    let (width, height) = (800, 800);
+    let mut universe = game_of_life::Universe::new(width, height);
 
     c.bench_function(
         "tick", 
@@ -14,27 +16,22 @@ fn benchmark(c: &mut Criterion) {
     }));
 }
 
-// fn universe_live_neighbours(b: &mut test::Bencher) {
-//     let (width, height) = (500, 500);
-//     let universe = game_of_life::Universe::new_rand(width, height);
-//     let cells = universe.get_cells();
-    
-//     let last_row = height - 1;
-//     let last_col = width - 1;
-//     let mid_row = height / 2;
-//     let mid_col = width / 2;
+#[allow(dead_code)]
+fn live_neighbours_benchmark(c: &mut Criterion) {
+    let (width, height) = (200, 200);
+    let universe = game_of_life::Universe::new(width, height);
+    let cells = universe.get_cells();
 
-//     let mut count = 0;
-//     b.iter(|| {
-//         count += game_of_life::Universe::live_neighbour_count(width, height, cells, 0, 0);
-//         count += game_of_life::Universe::live_neighbour_count(width, height, cells, 1, 1);
-//         count += game_of_life::Universe::live_neighbour_count(width, height, cells, 2, 2);
-//         count += game_of_life::Universe::live_neighbour_count(width, height, cells, mid_row, mid_col);
-//         count += game_of_life::Universe::live_neighbour_count(width, height, cells, last_row, last_col);
-//         count += game_of_life::Universe::live_neighbour_count(width, height, cells, last_row - 1, last_col - 1);
-//         count += game_of_life::Universe::live_neighbour_count(width, height, cells, last_row - 2, last_col - 2);
-//     })
-// }
+    c.bench_function(
+        "live_neighbours",
+        |b| b.iter(|| {
+            game_of_life::Universe::live_neighbour_count(width, height, cells, black_box(0), black_box(0));
+            game_of_life::Universe::live_neighbour_count(width, height, cells, black_box(0), black_box(2));
+            game_of_life::Universe::live_neighbour_count(width, height, cells, black_box(2), black_box(0));
+            game_of_life::Universe::live_neighbour_count(width, height, cells, black_box(height - 1), black_box(width - 1));
+        })
+    );
+}
 
-criterion_group!(benches, benchmark);
+criterion_group!(benches, live_neighbours_benchmark);
 criterion_main!(benches);
